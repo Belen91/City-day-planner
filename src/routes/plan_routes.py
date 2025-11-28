cb34ba1b6bac2a6e2262764f451d850e018b464a
Belen91 <belenromeroborrero@gmail.com>
2025-11-28 12:38:37 +0100

    add events to the resoults template

--- src/routes/plan_routes.py
+++ src/routes/plan_routes.py
@@ -3,11 +3,15 @@ from services.events_service import get_events
 from services.weather_service import get_weather_forecast
 
 def register_plan_routes(app):
+    
     @app.route('/plan', methods=['POST'])
-    def plans():
+    def plan():
         city = request.form['city']
         date = request.form['date']
 
+        if not city:
+            return render_template("index.html", error="Falta la ciudad")
+        
         weather_data = get_weather_forecast(city, date)
         events_data  = get_events(date, city) if date else []
 
@@ -15,9 +19,9 @@ def register_plan_routes(app):
             return render_template('index.html', error = "No se han encontrado datos para esta ciudad")
         
         return render_template(
-            'plans.html',
+            'plan.html',
             weather = weather_data,
-            city = city, 
-            date = date, 
+            city = city,
+            date = date,
             events = events_data
         )
